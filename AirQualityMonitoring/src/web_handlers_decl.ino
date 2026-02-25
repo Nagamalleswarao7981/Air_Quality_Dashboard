38bd86258f3e72941c1fd5cc5d95030dabebeacd
Nagamalleswarao7981 <nagamalleswarao@uniolabs.in>
2026-02-25 11:29:19 +0530

    Delete AirQualityMonitoring directory

--- AirQualityMonitoring/src/web_handlers_decl.ino
+++
@@ -1,8 +0,0 @@
-// web_handlers_decl.ino - Function declarations for web handlers
-// This file must be included AFTER config.h
-
-// External Web Server Instance
-extern WebServer server;
-
-// Function to setup all web routes
-void setupWeb();
